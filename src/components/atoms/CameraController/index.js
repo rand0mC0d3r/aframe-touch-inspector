1a554c36a6a48c26eff68baa864e8f4110a69c8d
Bogdan Dumitrescu <practiseo@gmail.com>
2020-11-18 09:33:58 +0100

    code

--- src/components/atoms/CameraController/index.js
+++ src/components/atoms/CameraController/index.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { IconButton } from '@material-ui/core';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearchMinus } from '@fortawesome/free-solid-svg-icons/faSearchMinus';
+import { faSearchPlus } from '@fortawesome/free-solid-svg-icons/faSearchPlus';
 
 const zoomY = 1.5;
 const zoomZ = 1.5;
-// let sceneCamera = window.AFRAME.scenes[0].camera;
 
 export default () => {
+  let sceneCamera = window.AFRAME.scenes[0].camera;
+
   const handleZoomOut = () => {
-    let position = window.AFRAME.scenes[0].camera.position;
-    window.AFRAME.scenes[0].camera.position.y = position.y + zoomY;
-    window.AFRAME.scenes[0].camera.position.z = position.z + zoomZ;
-    window.AFRAME.scenes[0].camera.lookAt(0, 0, 0);
+    sceneCamera.position.y = sceneCamera.position.y + zoomY;
+    sceneCamera.position.z = sceneCamera.position.z + zoomZ;
+    sceneCamera.lookAt(0, 0, 0);
   };
 
   const handleZoomIn = () => {
-    let position = window.AFRAME.scenes[0].camera.position;
-    window.AFRAME.scenes[0].camera.position.y = Math.max(position.y - zoomY, 0);
-    window.AFRAME.scenes[0].camera.position.z = position.z - zoomZ;
-    window.AFRAME.scenes[0].camera.lookAt(0, 0, 0);
+    sceneCamera.position.y = Math.max(sceneCamera.position.y - zoomY, 0);
+    sceneCamera.position.z = sceneCamera.position.z - zoomZ;
+    sceneCamera.lookAt(0, 0, 0);
   };
 
-  const handleZoomReset = () => {
-    window.AFRAME.scenes[0].camera.position.x = 0;
-    window.AFRAME.scenes[0].camera.position.y = 10;
-    window.AFRAME.scenes[0].camera.position.z = 20;
-    window.AFRAME.scenes[0].camera.lookAt(0, 0, 0);
-  };
+  // const handleZoomReset = () => {
+  //   sceneCamera.position.x = 0;
+  //   sceneCamera.position.y = 10;
+  //   sceneCamera.position.z = 20;
+  //   sceneCamera.lookAt(0, 0, 0);
+  // };
 
   return <React.Fragment>
-    <Button onClick={handleZoomIn} variant="contained">+</Button>
-    <Button onClick={handleZoomReset} variant="contained">0</Button>
-    <Button onClick={handleZoomOut} variant="contained">-</Button>
+    {/* <Button onClick={handleZoomReset} variant="contained">0</Button> */}
+    <IconButton onClick={handleZoomIn} title='Zoom in'>
+      <FontAwesomeIcon icon={faSearchPlus} size="sm" />
+    </IconButton>
+
+    <IconButton onClick={handleZoomOut} title='Zoom out'>
+      <FontAwesomeIcon icon={faSearchMinus} size="sm" />
+    </IconButton>
   </React.Fragment>;
 };
